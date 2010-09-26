e692d955efbbfe1b0033bb57011e4cfd37ca20c0
Nils Homer <nilshomer@gmail.com>
2010-09-26 12:13:43 -0700

    * scaffolding sfferr

--- src/sfferr/fmap_sfferr.h
+++ src/sfferr/fmap_sfferr.h
@@ -3,20 +3,22 @@
 
 /*! 
   @brief                structure to store the command line options for 'fmap sfferr'
-*/
+  */
 typedef struct {
-  char **argv;  /*! the command line argv structure */
-  int argc;  /*! the number of command line arguments passed */
-  char *fn_reads;  /*! the reads file name (-r) */
-  int32_t reads_format;  /*! the reads file format (-F)  */
-  double rand_sample_num;  /*! the fraction of reads to sample (-R) */
-  int32_t input_compr;  /*! the input compression type (-j and -z) */
-} fmap_sff_err_opt_t;
+    char **argv;  /*!< the command line argv structure */
+    int argc;  /*!< the number of command line arguments passed */
+    char *fn_fasta;  /*!< the fasta reference file name (-f) */
+    char *fn_sff;  /*!< the sff file name (-r) */
+    int32_t reads_format;  /*!< the reads file format (-F) */
+    double rand_sample_num;  /*!< the fraction of reads to sample (-R) */
+    int32_t input_compr;  /*!< the input compression type (-j and -z) */
+} fmap_sfferr_opt_t;
+
 /*! 
   @brief     main-like function for 'fmap sfferr'
   @param  argc  the number of arguments
   @param  argv  the argument list
   @return       0 if executed successful
-*/
-int fmap_map1(int argc, char *argv[]);
+  */
+int fmap_sfferr_main(int argc, char *argv[]);
 #endif
