4f92fa43c12936244e58da4b1fde7139d91841cc
Nils Homer <nils.homer@lifetech.com>
2011-06-30 15:50:51 -0700

    * more debugging indel traceback

--- src/sw/tmap_vsw16.c
+++ src/sw/tmap_vsw16.c
@@ -579,7 +579,7 @@ tmap_vsw16_sse2_get_path(const uint8_t *query, int32_t qlen,
   int32_t ti, qi;
   int32_t pen_gapoe, pen_gape;
   int32_t score, overflow = 0;
-  uint32_t ctype;
+  uint32_t ctype, prev_indel_start, next_indel_start;
   tmap_sw_path_t *p;
   tmap_vsw16_int_t h_cur;
   
@@ -620,13 +620,24 @@ tmap_vsw16_sse2_get_path(const uint8_t *query, int32_t qlen,
   if(tlen-1 != ti || qlen-1 != qi) {
       tmap_error("bug encountered", Exit, OutOfRange);
   }
-  
 
+  /*
+  for(qi=0;qi<qlen;qi++) {
+      fputc("ACGTN"[query[qi]], stderr);
+  }
+  fputc('\n', stderr);
+  for(ti=0;ti<tlen;ti++) {
+      fputc("ACGTN"[target[ti]], stderr);
+  }
+  fputc('\n', stderr);
+  */
+  
   // trackback
   p = path;
   ti = tlen-1; qi = q->qlen-1; // zero-based
   h_cur = __tmap_vsw16_get_matrix_cell(q, (ti < 0) ? 0 : ti, (qi < 0) ? 0 : qi) - q->zero_aln_score;
   ctype = TMAP_SW_FROM_M; 
+  prev_indel_start = 0;
   while(0 <= ti || 0 <= qi) {
       tmap_vsw16_int_t h_prev, e_prev, f_prev, h_cur_match, h_cur_indel;
       uint32_t ctype_match, ctype_indel;
@@ -642,11 +653,12 @@ tmap_vsw16_sse2_get_path(const uint8_t *query, int32_t qlen,
       score = __tmap_vsw16_get_query_profile_value(q, target[(ti < 0) ? 0 : ti], (qi < 0) ? 0 : qi);
       
       /*
-      fprintf(stderr, "ti=%d qi=%d ctype=%d h_cur=%d h_prev=%d e_prev=%d f_prev=%d score=%d tbase=%d qbase=%d\n",
+      fprintf(stderr, "ti=%d qi=%d ctype=%d h_cur=%d h_prev=%d e_prev=%d f_prev=%d score=%d tbase=%d qbase=%d prev_indel_start=%d\n",
               ti, qi, ctype, 
               h_cur, h_prev, e_prev, f_prev, score,
               target[(ti < 0) ? 0 : ti],
-              query[(qi < 0) ? 0 : qi]);
+              query[(qi < 0) ? 0 : qi],
+              prev_indel_start);
       */
 
       // one-based
@@ -733,21 +745,26 @@ tmap_vsw16_sse2_get_path(const uint8_t *query, int32_t qlen,
       }
 
       // compare indels
-      if(ctype == TMAP_SW_FROM_D && h_cur == e_prev - pen_gapoe) { // deletion start
+      next_indel_start = 0;
+      if((ctype == TMAP_SW_FROM_M || ctype == TMAP_SW_FROM_D) && h_cur == e_prev - pen_gapoe) { // deletion start
           h_cur_indel = e_prev;
           ctype_indel = TMAP_SW_FROM_D; 
+          next_indel_start = 1;
       }
-      else if((ctype == TMAP_SW_FROM_M || ctype == TMAP_SW_FROM_D) && h_cur == e_prev - pen_gape) { // deletion extension
+      else if(0 == prev_indel_start && (ctype == TMAP_SW_FROM_M || ctype == TMAP_SW_FROM_D) && h_cur == e_prev - pen_gape) { // deletion extension
           h_cur_indel = e_prev;
           ctype_indel = TMAP_SW_FROM_D; 
+          next_indel_start = 0;
       }
-      else if(ctype == TMAP_SW_FROM_I && h_cur == f_prev - pen_gapoe) { // insertion start
+      else if((ctype == TMAP_SW_FROM_M || ctype == TMAP_SW_FROM_I) && h_cur == f_prev - pen_gapoe) { // insertion start
           h_cur_indel = f_prev;
           ctype_indel = TMAP_SW_FROM_I; 
+          next_indel_start = 1;
       }
-      else if((ctype == TMAP_SW_FROM_M || ctype == TMAP_SW_FROM_I) && h_cur == f_prev - pen_gape) { // insertion extension
+      else if(0 == prev_indel_start && (ctype == TMAP_SW_FROM_M || ctype == TMAP_SW_FROM_I) && h_cur == f_prev - pen_gape) { // insertion extension
           h_cur_indel = f_prev;
           ctype_indel = TMAP_SW_FROM_I; 
+          next_indel_start = 0;
       }
 
       // check that we found a value
@@ -772,20 +789,24 @@ tmap_vsw16_sse2_get_path(const uint8_t *query, int32_t qlen,
           if(h_cur_match <= h_cur_indel) {
               h_cur = h_cur_indel;
               ctype = ctype_indel;
+              prev_indel_start = next_indel_start;
           }
           else {
               h_cur = h_cur_match;
               ctype = ctype_match;
+              prev_indel_start = 0;
           }
       }
       else { // left-justify, choose matches first 
           if(h_cur_indel <= h_cur_match) {
               h_cur = h_cur_match;
               ctype = ctype_match;
+              prev_indel_start = next_indel_start;
           }
           else {
               h_cur = h_cur_indel;
               ctype = ctype_indel;
+              prev_indel_start = 0;
           }
       }
 
@@ -800,7 +821,6 @@ tmap_vsw16_sse2_get_path(const uint8_t *query, int32_t qlen,
         default:
           tmap_error("bug encountered", Exit, OutOfRange);
       }
-
 
       // update p
       p->ctype = ctype;
