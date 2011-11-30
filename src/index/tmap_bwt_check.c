3d1a3fbe72a2812254c118dd23b62962ba254b2c
Nils Homer <nils.homer@lifetech.com>
2011-11-30 00:37:35 -0500

    * examining kmers sequences in reverse

--- src/index/tmap_bwt_check.c
+++ src/index/tmap_bwt_check.c
@@ -65,7 +65,7 @@ tmap_bwa_check_core(const char *fn_fasta, int32_t length, int32_t print_sa, int3
                   seqs[1][k] = 3 - seqs[0][i-k-1];
               }
               for(k=0;k<2;k++) {
-                  n[k] = tmap_bwt_match_exact(bwt, i, seqs[k], &sa);
+                  n[k] = tmap_bwt_match_exact_reverse(bwt, i, seqs[k], &sa);
                   if(0 == k) {
                       if(0 < n[k] && sa.k <= sa.l) {
                           sum += n[k];
