3a7e5b756dbbd755be01609806ad28dd3291e9b3
Nils Homer <nilshomer@gmail.com>
2011-12-02 14:18:07 -0500

    * adding a user hash API into the BWT, ontop of the BWT pre-built hash. * The old API uses the new API

--- src/index/tmap_bwt_match_hash.c
+++ src/index/tmap_bwt_match_hash.c
@@ -1,6 +1,10 @@
+/* Copyright (C) 2010 Ion Torrent Systems, Inc. All Rights Reserved */
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include "tmap_bwt.h"
 #include "../util/tmap_hash.h"
+#include "tmap_bwt_match.h"
 #include "tmap_bwt_match_hash.h"
 
 // TODO: change this if the size of the pacpos
@@ -54,16 +58,339 @@ tmap_bwt_match_hash_put(tmap_bwt_match_hash_t *h, uint32_t key, uint8_t c, uint3
 }
 
 uint32_t
-tmap_bwt_match_hash_get(tmap_bwt_match_hash_t *h, uint32_t key, uint8_t c)
+tmap_bwt_match_hash_get(tmap_bwt_match_hash_t *h, uint32_t key, uint8_t c, uint32_t *found)
 {
   tmap_hash_int_t iter;
   tmap_hash_t(tmap_bwt_match_hash) *hash = (tmap_hash_t(tmap_bwt_match_hash)*)(h->hash[c]);
   // get the iter
   iter = tmap_hash_get(tmap_bwt_match_hash, hash, key); 
   // check it was found
-  if(tmap_hash_end(hash) == iter) {
+  if(tmap_hash_end(hash) == iter) { // not found
+      *found = 0;
       return UINT32_MAX;
   }
-  // return the value
-  return (uint32_t)(tmap_hash_val(hash, iter));
+  else {
+      *found = 1;
+      // return the value
+      return (uint32_t)(tmap_hash_val(hash, iter));
+  }
+}
+
+inline void
+tmap_bwt_match_occ_hash(const tmap_bwt_t *bwt, tmap_bwt_match_occ_t *prev, uint8_t c, 
+                          tmap_bwt_match_occ_t *next, tmap_bwt_match_hash_t *hash)
+{
+  uint32_t offset;
+  offset = (NULL == prev) ? 0 : prev->offset;
+  if(bwt->hash_width <= offset) { // do not use the bwt hash
+      uint32_t prev_k, found_k;
+      prev_k = (NULL == prev) ? 0 : prev->k;
+      // test the user "hash"
+      next->k = tmap_bwt_match_hash_get(hash, prev_k-1, c, &found_k);
+      // compute the k, value, if necessary
+      if(0 == found_k) {
+          next->k = tmap_bwt_occ(bwt, prev_k-1, c) + bwt->L2[c] + 1;
+          // put it in the hash
+          tmap_bwt_match_hash_put(hash, prev_k-1, c, next->k);
+      }
+      next->offset = offset + 1;
+      next->hi = UINT32_MAX;
+      next->l = UINT32_MAX; 
+  }
+  else { // use the bwt hash
+      uint64_t prev_hi = (NULL == prev) ? 0 : prev->hi;
+      next->offset = offset + 1;
+      next->hi = (prev_hi << 2) + c;
+      next->k = bwt->hash_k[next->offset-1][next->hi];
+      next->l = UINT32_MAX; 
+  }
+}
+
+inline void
+tmap_bwt_match_2occ_hash(const tmap_bwt_t *bwt, tmap_bwt_match_occ_t *prev, uint8_t c, 
+                    tmap_bwt_match_occ_t *next, tmap_bwt_match_hash_t *hash)
+{
+  uint32_t offset;
+  offset = (NULL == prev) ? 0 : prev->offset;
+  if(bwt->hash_width <= offset) { // do not use the bwt hash
+      uint32_t prev_k, prev_l, found_k, found_l;
+      prev_k = (NULL == prev) ? 0 : prev->k;
+      prev_l = (NULL == prev) ? bwt->seq_len : prev->l;
+      // test the user "hash"
+      next->k = tmap_bwt_match_hash_get(hash, prev_k-1, c, &found_k); // compute k
+      next->l = tmap_bwt_match_hash_get(hash, prev_l, c, &found_l); // compute -l
+      // compute the k value, if necessary
+      if(0 == found_k) { // for k
+          next->k = tmap_bwt_occ(bwt, prev_k-1, c) + bwt->L2[c] + 1;
+          // put it in the hash
+          tmap_bwt_match_hash_put(hash, prev_k-1, c, next->k);
+      }
+      // compute the l value, if necessary
+      if(0 == found_l) { // for l
+          next->l = tmap_bwt_occ(bwt, prev_l, c) + bwt->L2[c];
+          // put it in the hash
+          tmap_bwt_match_hash_put(hash, prev_l, c, next->l);
+      }
+      next->offset = offset + 1;
+      next->hi = UINT32_MAX;
+  }
+  else { // use the bwt hash
+      uint64_t prev_hi = (NULL == prev) ? 0 : prev->hi;
+      next->offset = offset + 1;
+      next->hi = (prev_hi << 2) + c;
+      next->k = bwt->hash_k[next->offset-1][next->hi];
+      next->l = bwt->hash_l[next->offset-1][next->hi];
+  }
+}
+
+inline void
+tmap_bwt_match_occ4_hash(const tmap_bwt_t *bwt, tmap_bwt_match_occ_t *prev, 
+                    tmap_bwt_match_occ_t next[4], tmap_bwt_match_hash_t *hash)
+{
+  uint32_t i, offset;
+  offset = (NULL == prev) ? 0 : prev->offset;
+  if(bwt->hash_width <= offset) { // do not use the bwt hash
+      uint32_t cntk[4];
+      uint32_t prev_k, found_k;
+      prev_k = (NULL == prev) ? 0 : prev->k;
+      // test the user "hash" for k
+      for(i=0;i<4;i++) {
+          next[i].k = tmap_bwt_match_hash_get(hash, prev_k-1, i, &found_k); // compute k
+          if(0 == found_k) { // for k
+              break;
+          }
+      } // TODO: should we use tmap_bwt_occ if we break out of this loop?
+      // compute the k value, if necessary
+      if(0 == found_k) {
+          tmap_bwt_occ4(bwt, prev_k-1, cntk);
+          for(i=0;i<4;i++) {
+              next[i].k = cntk[i] + bwt->L2[i] + 1;
+              // put it in the hash
+              tmap_bwt_match_hash_put(hash, prev_k-1, i, next[i].k);
+          }
+      }
+      for(i=0;i<4;i++) {
+          next[i].offset = offset + 1;
+          next[i].hi = UINT32_MAX;
+          // ignore k;
+          next[i].l = UINT32_MAX;
+      }
+  }
+  else { // use the bwt hash
+      uint64_t prev_hi = (NULL == prev) ? 0 : prev->hi;
+      for(i=0;i<4;i++) {
+          next[i].offset = offset + 1;
+          next[i].hi = (prev_hi << 2) + i;
+          next[i].k = bwt->hash_k[next[i].offset-1][next[i].hi];
+          next[i].l = UINT32_MAX;
+      }
+  }
+}
+
+inline void
+tmap_bwt_match_2occ4_hash(const tmap_bwt_t *bwt, tmap_bwt_match_occ_t *prev, 
+                            tmap_bwt_match_occ_t next[4], tmap_bwt_match_hash_t *hash)
+{
+  uint32_t i, offset;
+  offset = (NULL == prev) ? 0 : prev->offset;
+  if(bwt->hash_width <= offset) { // do not use the bwt hash
+      uint32_t cntk[4], cntl[4];
+      uint32_t prev_k, prev_l, found_k, found_l;
+      prev_k = (NULL == prev) ? 0 : prev->k;
+      prev_l = (NULL == prev) ? bwt->seq_len : prev->l;
+      // test the user "hash" for k
+      for(i=0;i<4;i++) {
+          next[i].k = tmap_bwt_match_hash_get(hash, prev_k-1, i, &found_k); // compute k
+          if(0 == found_k) { // for k
+              break;
+          }
+          next[i].l = tmap_bwt_match_hash_get(hash, prev_l, i, &found_l); // compute k
+          if(0 == found_l) { // for l
+              break;
+          }
+      } // TODO: should we use tmap_bwt_occ if we break out of this loop?
+      // compute the k value, if necessary
+      if(0 == found_k || 0 == found_l) {
+          tmap_bwt_2occ4(bwt, prev_k-1, prev_l, cntk, cntl);
+          for(i=0;i<4;i++) {
+              next[i].k = cntk[i] + bwt->L2[i] + 1;
+              next[i].l = cntl[i] + bwt->L2[i];
+              // put it in the hash
+              tmap_bwt_match_hash_put(hash, prev_k-1, i, next[i].k);
+              tmap_bwt_match_hash_put(hash, prev_l, i, next[i].l);
+          }
+      }
+      for(i=0;i<4;i++) {
+          next[i].offset = offset + 1;
+          next[i].hi = UINT32_MAX;
+          // ignore k and l
+      }
+  }
+  else { // use the bwt hash
+      uint64_t prev_hi = (NULL == prev) ? 0 : prev->hi;
+      for(i=0;i<4;i++) {
+          next[i].offset = offset + 1;
+          next[i].hi = (prev_hi << 2) + i;
+          next[i].k = bwt->hash_k[next[i].offset-1][next[i].hi];
+          next[i].l = bwt->hash_l[next[i].offset-1][next[i].hi];
+      }
+  }
+}
+
+void
+tmap_bwt_match_cal_width_forward_hash(const tmap_bwt_t *bwt, int len, const char *str, 
+                                      tmap_bwt_match_width_t *width, tmap_bwt_match_hash_t *hash)
+{
+  // 'width[i]' is the lower bound of the number of differences in str[i,len]
+  tmap_bwt_match_occ_t prev, next;
+  int32_t i, bid;
+  
+  prev.k = 0; prev.l = bwt->seq_len;
+  prev.offset = 0;
+  prev.hi = 0;
+
+  bid = 0;
+  for(i=len-1;0<=i;i--) {
+      uint8_t c = (int)str[i];
+      if(c < 4) {
+          tmap_bwt_match_2occ_hash(bwt, &prev, c, &next, hash);
+      }
+      if(next.l < next.k || 3 < c) { // new width
+          next.k = 0;
+          next.l = bwt->seq_len;
+          next.offset = 0;
+          next.hi = 0;
+          bid++;
+      }
+      width[i].w = next.l - next.k + 1;
+      width[i].bid = bid;
+      prev = next;
+  }
+}
+
+void
+tmap_bwt_match_cal_width_reverse_hash(const tmap_bwt_t *bwt, int len, const char *str, 
+                                 tmap_bwt_match_width_t *width, tmap_bwt_match_hash_t *hash)
+{
+  // 'width[i]' is the lower bound of the number of differences in str[0,i]
+  tmap_bwt_match_occ_t prev, next;
+  int32_t i, bid;
+  
+  prev.k = 0; prev.l = bwt->seq_len;
+  prev.offset = 0;
+  prev.hi = 0;
+
+  bid = 0;
+  for(i=0;i<len;i++) {
+      uint8_t c = (int)str[i];
+      if(c < 4) {
+          tmap_bwt_match_2occ_hash(bwt, &prev, c, &next, hash);
+      }
+      if(next.l < next.k || 3 < c) { // new width
+          next.k = 0;
+          next.l = bwt->seq_len;
+          next.offset = 0;
+          next.hi = 0;
+          bid++;
+      }
+      width[i].w = next.l - next.k + 1;
+      width[i].bid = bid;
+      prev = next;
+  }
+  width[len].w = 0;
+  width[len].bid = ++bid;
+}
+
+uint32_t
+tmap_bwt_match_exact_hash(const tmap_bwt_t *bwt, int len, const uint8_t *str, 
+                          tmap_bwt_match_occ_t *match_sa, tmap_bwt_match_hash_t *hash)
+{
+  int32_t i;
+  uint8_t c = 0;
+  tmap_bwt_match_occ_t prev, next;
+
+  prev.k = 0; prev.l = bwt->seq_len;
+  prev.offset = 0;
+  prev.hi = 0;
+
+  for(i=0;i<len;i++) {
+      c = str[i];
+      if(3 < c) { 
+          prev.offset++;
+          prev.k = prev.l + 1;
+          break;
+      }
+      tmap_bwt_match_2occ_hash(bwt, &prev, c, &next, hash);
+      prev = next;
+      if(next.k > next.l) break; // no match
+  }
+  if(NULL != match_sa) {
+      (*match_sa) = prev;
+  }
+  if(3 < c || prev.k > prev.l) return 0; // no match
+  return prev.l - prev.k + 1;
+}
+
+uint32_t
+tmap_bwt_match_exact_reverse_hash(const tmap_bwt_t *bwt, int len, const uint8_t *str, 
+                             tmap_bwt_match_occ_t *match_sa, tmap_bwt_match_hash_t *hash)
+{
+  int32_t i;
+  uint8_t c = 0;
+  tmap_bwt_match_occ_t prev, next;
+
+  prev.k = 0; prev.l = bwt->seq_len;
+  prev.offset = 0;
+  prev.hi = 0;
+
+  for(i=len-1;0<=i;i--) {
+      c = str[i];
+      if(3 < c) { 
+          prev.offset++; 
+          prev.k = prev.l + 1;
+          break;
+      }
+      tmap_bwt_match_2occ_hash(bwt, &prev, c, &next, hash);
+      prev = next;
+      if(next.k > next.l) break; // no match
+  }
+  if(NULL != match_sa) {
+      (*match_sa) = prev;
+  }
+  if(3 < c || prev.k > prev.l) return 0; // no match
+  return prev.l - prev.k + 1;
+}
+
+uint32_t
+tmap_bwt_match_exact_alt_hash(const tmap_bwt_t *bwt, int len, const uint8_t *str, 
+                         tmap_bwt_match_occ_t *match_sa, tmap_bwt_match_hash_t *hash)
+{
+  int i;
+  tmap_bwt_match_occ_t next;
+
+  for(i=0;i<len;i++) {
+      uint8_t c = str[i];
+      if(c > 3) return 0; // there is an N here. no match
+      tmap_bwt_match_2occ_hash(bwt, match_sa, c, &next, hash);
+      (*match_sa) = next;
+      if(next.k > next.l) return 0; // no match
+  }
+  return match_sa->l - match_sa->k + 1;
+}
+
+uint32_t
+tmap_bwt_match_exact_alt_reverse_hash(const tmap_bwt_t *bwt, int len, const uint8_t *str, 
+                                 tmap_bwt_match_occ_t *match_sa, tmap_bwt_match_hash_t *hash)
+{
+  int i;
+  tmap_bwt_match_occ_t next;
+
+  for(i=len-1;0<=i;i--) {
+      uint8_t c = str[i];
+      if(c > 3) return 0; // there is an N here. no match
+      tmap_bwt_match_2occ_hash(bwt, match_sa, c, &next, hash);
+      (*match_sa) = next;
+      if(next.k > next.l) return 0; // no match
+  }
+  return match_sa->l - match_sa->k + 1;
 }
--- src/index/tmap_bwt_match_hash.h
+++ src/index/tmap_bwt_match_hash.h
@@ -1,9 +1,15 @@
+/* Copyright (C) 2010 Ion Torrent Systems, Inc. All Rights Reserved */
 #ifndef TMAP_BWT_MATCH_HASH_H_
 #define TMAP_BWT_MATCH_HASH_H_
 
 /*!
-  The BWT hash wrapper
- */
+  The BWT hash wrapper and an API for BWT Index Lookups with further hashing
+  @details  This API facilitates a secondary hash into the BWT
+  */
+
+/*!
+  Hash structure
+  */
 typedef struct {
    void *hash[4]; /*! the hash used by bwt match for each possible next base, the type is defined in the source */ 
 } tmap_bwt_match_hash_t;
@@ -41,9 +47,113 @@ tmap_bwt_match_hash_put(tmap_bwt_match_hash_t *h, uint32_t key, uint8_t c, uint3
   @param  h  the hash 
   @param  key  the hash key
   @param  c    the base in integer format
+  @param  found  1 if the it was contained in the hash, 0 otherwise
   @return the hash value, or UINT32_MAX if not found
  */
 uint32_t
-tmap_bwt_match_hash_get(tmap_bwt_match_hash_t *h, uint32_t key, uint8_t c);
+tmap_bwt_match_hash_get(tmap_bwt_match_hash_t *h, uint32_t key, uint8_t c, uint32_t *found);
+
+/*! 
+  analagous function to tmap_bwt_occ but using a hash
+  @param  bwt      pointer to the bwt structure 
+  @param  prev     pointer to the previous match structure
+  @param  c        base in two-bit integer format
+  @param  next     pointer to the next match structure
+  @param  hash     a occurence array hash
+  @details         this will not set the upper occurrence of the SA interval
+  */
+inline void
+tmap_bwt_match_occ_hash(const tmap_bwt_t *bwt, tmap_bwt_match_occ_t *prev, uint8_t c, tmap_bwt_match_occ_t *next, tmap_bwt_match_hash_t *hash);
+
+/*! 
+  analagous function to tmap_bwt_2occ but using a hash
+  @param  bwt      pointer to the bwt structure 
+  @param  prev     pointer to the previous match structure
+  @param  c        base in two-bit integer format
+  @param  next     pointer to the next match structure
+  @param  hash     a occurence array hash
+  @details         this will not set the upper occurrences of the SA interval
+  */
+inline void
+tmap_bwt_match_2occ_hash(const tmap_bwt_t *bwt, tmap_bwt_match_occ_t *prev, uint8_t c, tmap_bwt_match_occ_t *next, tmap_bwt_match_hash_t *hash);
+
+/*! 
+  analagous function to tmap_bwt_occ4 but using a hash
+  @param  bwt      pointer to the bwt structure 
+  @param  prev     pointer to the previous match structure
+  @param  next     pointer to the next match structure
+  @param  hash     a occurence array hash
+  */
+inline void
+tmap_bwt_match_occ4_hash(const tmap_bwt_t *bwt, tmap_bwt_match_occ_t *prev, tmap_bwt_match_occ_t next[4], tmap_bwt_match_hash_t *hash);
+
+/*! 
+  analagous function to tmap_bwt_2occ4 but using a hash
+  @param  bwt      pointer to the bwt structure 
+  @param  prev     pointer to the previous match structure
+  @param  next     pointer to the next match structure
+  */
+inline void
+tmap_bwt_match_2occ4_hash(const tmap_bwt_t *bwt, tmap_bwt_match_occ_t *prev, tmap_bwt_match_occ_t next[4], tmap_bwt_match_hash_t *hash);
+
+/*! 
+  calculates a lower bound on the number of mismatches in the string for each interval [i,len-1]
+  @param  bwt    the reverse BWT being used to search
+  @param  len    the length of the string
+  @param  str    the string with bases in integer format
+  @param  width  array of widths, one for each interval [i,len-1], for 0 <= i < len
+  @param  hash   a occurence array hash
+*/
+void
+tmap_bwt_match_cal_width_forward_hash(const tmap_bwt_t *bwt, int len, const char *str, tmap_bwt_match_width_t *width, tmap_bwt_match_hash_t *hash);
+
+/*! 
+  calculates a lower bound on the number of mismatches in the string for each interval [0,i]
+  @param  bwt    the reverse BWT being used to search
+  @param  len    the length of the string
+  @param  str    the string with bases in integer format
+  @param  width  array of widths, one for each interval [0,i], for 0 <= i < len
+  @param  hash   a occurence array hash
+*/
+void
+tmap_bwt_match_cal_width_reverse_hash(const tmap_bwt_t *bwt, int len, const char *str, tmap_bwt_match_width_t *width, tmap_bwt_match_hash_t *hash);
+
+/*! 
+  computes the SA interval for the given sequence (if any), using forward search
+  @param  bwt       pointer to the bwt structure 
+  @param  len       the length of the sequence
+  @param  str       the DNA sequence in 2-bit format
+  @param  match_sa  pointer to the match structure to be returned
+  @param  hash      a occurence array hash
+  @return           the size of the SA interval, 0 if none found
+  */
+uint32_t
+tmap_bwt_match_exact_hash(const tmap_bwt_t *bwt, int len, const uint8_t *str, tmap_bwt_match_occ_t *match_sa, tmap_bwt_match_hash_t *hash);
+
+/*! 
+  computes the SA interval for the given sequence (if any), using forward search
+  @param  bwt       pointer to the bwt structure 
+  @param  len       the length of the sequence
+  @param  str       the DNA sequence in 2-bit format
+  @param  match_sa  pointer to the match structure to be returned
+  @param  hash      a occurence array hash
+  @return           the size of the SA interval, 0 if none found
+  @details          the search will be started at SA interval [k0,l0], with the results returned as [k0,l0]
+  */
+uint32_t
+tmap_bwt_match_exact_alt_hash(const tmap_bwt_t *bwt, int len, const uint8_t *str, tmap_bwt_match_occ_t *match_sa, tmap_bwt_match_hash_t *hash);
+
+/*! 
+  computes the SA interval for the given sequence (if any), using forward search
+  @param  bwt       pointer to the bwt structure 
+  @param  len       the length of the sequence
+  @param  str       the DNA sequence in 2-bit format
+  @param  match_sa  pointer to the match structure to be returned
+  @param  hash      a occurence array hash
+  @return           the size of the SA interval, 0 if none found
+  @details          the search will be started at SA interval [k0,l0], with the results returned as [k0,l0]
+  */
+uint32_t
+tmap_bwt_match_exact_alt_reverse_hash(const tmap_bwt_t *bwt, int len, const uint8_t *str, tmap_bwt_match_occ_t *match_sa, tmap_bwt_match_hash_t *hash);
 
 #endif
