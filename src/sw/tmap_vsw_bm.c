ecae5194c5dee22be71c58ef50f42fffd58dbf24
Nils Homer <nilshomer@gmail.com>
2012-01-24 09:36:53 -0500

    * adding sub iteration to 'vswbm'; fortunately, this has very little impact on run time

--- src/sw/tmap_vsw_bm.c
+++ src/sw/tmap_vsw_bm.c
@@ -23,7 +23,7 @@
 #ifdef ENABLE_TMAP_DEBUG_FUNCTIONS
 void
 tmap_vsw_bm_core(int32_t seq_len, int32_t tlen, int32_t n_iter,
-                 int32_t use_sw)
+                 int32_t n_sub_iter, int32_t use_sw)
 {
   int32_t i, j, k;
   tmap_vsw_query_t *vsw_query = NULL;
@@ -63,7 +63,7 @@ tmap_vsw_bm_core(int32_t seq_len, int32_t tlen, int32_t n_iter,
 
   int32_t front = (tlen - seq_len) / 2;
   int32_t end = tlen - seq_len - front;
-  for(i=0;i<n_iter;i++) {
+  while(i<n_iter) {
       tmap_map_sam_t tmp_sam;
       int32_t overflow, n_best;
       for(j=k=0;j<front;j++,k++) {
@@ -75,24 +75,26 @@ tmap_vsw_bm_core(int32_t seq_len, int32_t tlen, int32_t n_iter,
       for(j=0;j<end;j++,k++) {
           target[k] = (uint8_t)(4*tmap_rand_get(rand));
       }
-      if(0 == use_sw) {
-          // initialize the bounds
-          tmp_sam.query_start = tmp_sam.query_end = 0;
-          tmp_sam.target_start = tmp_sam.target_end = 0;
-          // run the vsw
-          tmap_vsw_sse2(vsw_query, seq, seq_len,
-                        target, tlen,
-                        softclip_start, softclip_end,
-                        vsw_opt, 
-                        &tmp_sam.score_fwd, &tmp_sam.score_rev,
-                        &tmp_sam.query_start, &tmp_sam.query_end,
-                        &tmp_sam.target_start, &tmp_sam.target_end,
-                        &overflow, &n_best, opt->score_thr, 0);
-      }
-      else {
-          tmap_sw_clipping_core(seq, seq_len, target, tlen,
-                                &ap, softclip_start, softclip_end,
-                                NULL, NULL, 0);
+      for(j=0;j<n_sub_iter&&i<n_iter;j++,i++) {
+          if(0 == use_sw) {
+              // initialize the bounds
+              tmp_sam.query_start = tmp_sam.query_end = 0;
+              tmp_sam.target_start = tmp_sam.target_end = 0;
+              // run the vsw
+              tmap_vsw_sse2(vsw_query, seq, seq_len,
+                            target, tlen,
+                            softclip_start, softclip_end,
+                            vsw_opt, 
+                            &tmp_sam.score_fwd, &tmp_sam.score_rev,
+                            &tmp_sam.query_start, &tmp_sam.query_end,
+                            &tmp_sam.target_start, &tmp_sam.target_end,
+                            &overflow, &n_best, opt->score_thr, 0);
+          }
+          else {
+              tmap_sw_clipping_core(seq, seq_len, target, tlen,
+                                    &ap, softclip_start, softclip_end,
+                                    NULL, NULL, 0);
+          }
       }
   }
 
@@ -108,7 +110,8 @@ tmap_vsw_bm_core(int32_t seq_len, int32_t tlen, int32_t n_iter,
 }
 
 static int
-usage(int32_t seq_len, int32_t tlen, int32_t n_iter, int32_t use_sw)
+usage(int32_t seq_len, int32_t tlen, int32_t n_iter, 
+      int32_t n_sub_iter, int32_t use_sw)
 {
   tmap_file_fprintf(tmap_file_stderr, "\n");
   tmap_file_fprintf(tmap_file_stderr, "Usage: %s vswbm [options]", PACKAGE);
@@ -117,6 +120,7 @@ usage(int32_t seq_len, int32_t tlen, int32_t n_iter, int32_t use_sw)
   tmap_file_fprintf(tmap_file_stderr, "         -q INT      the query length [%d]\n", seq_len);
   tmap_file_fprintf(tmap_file_stderr, "         -t INT      the target length [%d]\n", tlen);
   tmap_file_fprintf(tmap_file_stderr, "         -n INT      the number of iterations [%d]\n", n_iter);
+  tmap_file_fprintf(tmap_file_stderr, "         -N INT      the number of re-evaluations of the same query/target combination [%d]\n", n_sub_iter);
   tmap_file_fprintf(tmap_file_stderr, "         -S          use the non-vectorized Smith Waterman [%s]\n",
                     (1 == use_sw) ? "true" : "false");
   tmap_file_fprintf(tmap_file_stderr, "Options (optional):\n");
@@ -132,10 +136,11 @@ tmap_vswbm_main(int argc, char *argv[])
   int32_t seq_len = 150;
   int32_t tlen = 256; 
   int32_t n_iter = 1000;
+  int32_t n_sub_iter = 1;
   int32_t use_sw = 0;
   int c;
 
-  while((c = getopt(argc, argv, "q:t:n:Sh")) >= 0) {
+  while((c = getopt(argc, argv, "q:t:n:N:Sh")) >= 0) {
       switch(c) {
         case 'q':
           seq_len = atoi(optarg); break;
@@ -143,21 +148,23 @@ tmap_vswbm_main(int argc, char *argv[])
           tlen = atoi(optarg); break;
         case 'n':
           n_iter = atoi(optarg); break;
+        case 'N':
+          n_sub_iter = atoi(optarg); break;
         case 'S':
           use_sw = 1; break;
         case 'h':
         default:
-          return usage(seq_len, tlen, n_iter, use_sw);
+          return usage(seq_len, tlen, n_iter, n_sub_iter, use_sw);
       }
   }
   if(argc != optind) {
-      return usage(seq_len, tlen, n_iter, use_sw);
+      return usage(seq_len, tlen, n_iter, n_sub_iter, use_sw);
   }
 
   tmap_progress_set_verbosity(1);
   tmap_progress_print2("starting benchmark");
 
-  tmap_vsw_bm_core(seq_len, tlen, n_iter, use_sw);
+  tmap_vsw_bm_core(seq_len, tlen, n_iter, n_sub_iter, use_sw);
   
   tmap_progress_print2("ending benchmark");
 
