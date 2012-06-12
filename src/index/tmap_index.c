6846e348ab12675de3d030587727f4c98bb3e81c
Nils Homer <nils.homer@lifetech.com>
2012-06-12 10:01:07 -0400

    * small format change

--- src/index/tmap_index.c
+++ src/index/tmap_index.c
@@ -113,7 +113,7 @@ usage(tmap_index_opt_t *opt)
   tmap_file_fprintf(tmap_file_stderr, "         -o INT      the occurrence interval (use %d, %d, %d, ...) [%d]\n", 
                     TMAP_BWT_OCC_MOD, TMAP_BWT_OCC_MOD*2, TMAP_BWT_OCC_MOD*3, opt->occ_interval);
   tmap_file_fprintf(tmap_file_stderr, "         -w INT      the k-mer occurrence hash width [%d]\n", opt->hash_width);
-  tmap_file_fprintf(tmap_file_stderr, "         -i INT      the suffix array interval (use 1, 2, 4, ...)[%d]\n", opt->sa_interval);
+  tmap_file_fprintf(tmap_file_stderr, "         -i INT      the suffix array interval (use 1, 2, 4, ...) [%d]\n", opt->sa_interval);
   tmap_file_fprintf(tmap_file_stderr, "         -a STRING   override BWT construction algorithm:\n");
   tmap_file_fprintf(tmap_file_stderr, "                     \t\"bwtsw\" (large genomes)\n");
   tmap_file_fprintf(tmap_file_stderr, "                     \t\"is\" (short genomes)\n");
